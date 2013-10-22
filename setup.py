7637c3afd7fdb6172b66d056aee44b89bced1860
Bogdan Sulima <bogdan.sulima@gmail.com>
2013-10-22 13:36:22 +0200

    patched version for pip-git installation

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from distutils.core import setup
 
 setup(
     name='commandr',
-    version='1.2.0',
+    version='1.2.0-patched',
     packages=['commandr'],
     author='Kevin Ballard',
     author_email='kevin@tellapart.com',
